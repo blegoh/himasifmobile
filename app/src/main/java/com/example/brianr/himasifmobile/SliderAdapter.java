fd0df0ee9ad85b47adb5fa7cc8d6085e0068f907
L007 <antoeldi@gmail.com>
2018-01-11 11:14:26 +0700

    tes

--- app/src/main/java/com/example/brianr/himasifmobile/SliderAdapter.java
+++ app/src/main/java/com/example/brianr/himasifmobile/SliderAdapter.java
@@ -14,6 +14,7 @@ import android.widget.TextView;
  */
 
 public class SliderAdapter extends PagerAdapter {
+    //tes
     private int[] image_resources = {
             R.mipmap.eldi,
             R.mipmap.umroh,
